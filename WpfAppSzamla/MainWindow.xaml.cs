81fa0acb44693e651604e410dd8c4bdfa9bb7def
Hagymási Gyula <oktato.ber@gmail.com>
2023-12-11 19:49:23 +0100

    Belekerült a törlés és módosítás funkció!

--- WpfAppSzamla/MainWindow.xaml.cs
+++ WpfAppSzamla/MainWindow.xaml.cs
@@ -42,11 +42,10 @@ namespace WpfAppSzamla
 
         private void btnFelvesz_Click(object sender, RoutedEventArgs e)
         {
-            kosarErtek += int.Parse(txtAr.Text) * sliMennyiseg.Value;
-            labSzum.Content = kosarErtek+" Ft";
 
             Tetel ujtetel = new Tetel(txtNev.Text, int.Parse(txtAr.Text), cbEgyseg.SelectedItem.ToString(), sliMennyiseg.Value);
             tetelek.Add(ujtetel);
+            labSzum.Content = TetelekOsszege() + " Ft";
 
             //Módosítás esetén (új elem, törlés, tartalom módosítás), szeretném a változást a DataGrid-ben látni akkor két út van:
             //1) List<Tetel> tetelek = new List<Tetel>(); esetben
@@ -72,20 +71,67 @@ namespace WpfAppSzamla
         private void btnBetolt_Click(object sender, RoutedEventArgs e)
         {
             tetelek.Clear();
-            kosarErtek = 0;
             var sorok = File.ReadAllLines("lista.csv");
             foreach (var sor in sorok)
             {
                 var mezoErtekek = sor.Split(';');
                 Tetel ujTetel = new Tetel(mezoErtekek[0], int.Parse(mezoErtekek[1]), mezoErtekek[2], double.Parse(mezoErtekek[3]));
                 tetelek.Add(ujTetel);
-                kosarErtek += int.Parse(mezoErtekek[1]) * double.Parse(mezoErtekek[3]);
             }
-            labSzum.Content = kosarErtek + " Ft";
+            labSzum.Content = TetelekOsszege() + " Ft";
             //Ha List<Tetel> tetelek = new List<Tetel>(); használom akkor kell:
             dgTetelek.ItemsSource = null;
             dgTetelek.ItemsSource = tetelek;
 
+        }
+
+        private void btnTörlés_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgTetelek.SelectedIndex < 0)
+            {
+                MessageBox.Show("Előbb válasszon terméket!");
+            }
+            else
+            {
+                tetelek.RemoveAt(dgTetelek.SelectedIndex);
+                dgTetelek.ItemsSource = null;
+                dgTetelek.ItemsSource = tetelek;
+                labSzum.Content = TetelekOsszege() + " Ft";
+            }
+        }
+
+        private double TetelekOsszege()
+        {
+            kosarErtek = 0;
+            foreach (var aktTetel in tetelek)
+            {
+                kosarErtek += aktTetel.EgysegAr * aktTetel.Mennyiseg;
+            }
+            return kosarErtek;
+        }
+
+        private void dgTetelek_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (dgTetelek.SelectedIndex < 0)
+            {
+                MessageBox.Show("Előbb válasszon terméket!");
+            }
+            else
+            {
+                var valasztott = dgTetelek.SelectedItem as Tetel;
+                txtNev.Text = valasztott.TermekNev;
+                txtAr.Text = valasztott.EgysegAr + "";
+                sliMennyiseg.Value = valasztott.Mennyiseg;
+                cbEgyseg.SelectedItem = valasztott.Egyseg;
+
+                tetelek.Remove(valasztott);
+
+                labSzum.Content = TetelekOsszege() + " Ft";
+
+                dgTetelek.ItemsSource = null;
+                dgTetelek.ItemsSource = tetelek;
+
+            }
         }
     }
 }
